d044ac61ce50784af025d765b19e2204a3388523
christiankriegsheim <39035968+christiankriegsheim@users.noreply.github.com>
2021-08-31 11:24:28 +0200

    Initial Content

---
+++ .eleventy.js
@@ -0,0 +1,31 @@
+module.exports = function(eleventyConfig) {
+
+  const moment = require("moment");
+
+  eleventyConfig.addPassthroughCopy("./src/js/main-min.js");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+
+
+  // date filter (localized)
+  eleventyConfig.addNunjucksFilter("date", function (date, format, locale) {
+    locale = locale ? locale : "en";
+    moment.locale(locale);
+    return moment(date).format(format);
+  });
+
+  //©copyrights year output
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+
+
+  // You can return your Config object (optional).
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      data: "_data"
+    }
+  };
+
+};
